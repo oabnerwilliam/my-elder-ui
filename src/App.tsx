2e0fa16d564a28b0fe230442dc239a2c3901dad1
oabnerwilliam <abnerwilliam026@gmail.com>
2025-09-18 23:27:56 -0300

    atualizando o app

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,11 @@ import { Navbar } from "./layout/navbar/Navbar"
 import { HomePage } from "./screens/home/HomePage"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { Idosos } from "./screens/idosos/Idosos"
+import { LoginPage } from "./screens/login/LoginPage"
+import { SignUpPage } from "./screens/signup/SignUpPage"
+import { Cuidadores } from "./screens/cuidadores/Cuidadores"
+import { MatchesPage } from "./screens/matches/MatchesPage"
+import { PageContainer } from "./layout/container/PageContainer"
 
 export const App = () => {
   return (
@@ -10,10 +15,16 @@ export const App = () => {
       <Router>
         <Container>
           <Navbar />
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/idosos" element={<Idosos />} />
-          </Routes>
+          <PageContainer>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/idosos" element={<Idosos />} />
+              <Route path="/cuidadores" element={<Cuidadores />} />
+              <Route path="/matches" element={<MatchesPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/cadastro" element={<SignUpPage />} />
+            </Routes>
+          </PageContainer>
         </Container>
       </Router>
     </>
