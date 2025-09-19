2716bc60d72a861d794b29ec370a186484505a2f
oabnerwilliam <abnerwilliam026@gmail.com>
2025-09-18 23:26:48 -0300

    modificando página de idosos + criando a de cuidadores

--- src/screens/cuidadores/Cuidadores.tsx
+++ src/screens/cuidadores/Cuidadores.tsx
@@ -1,7 +1,6 @@
 import { useMutation, useQuery } from "@apollo/client/react"
-import { idososQueryGql } from "../../utils/graphql/queries/idosoQueries"
-import avatarMasc from "../../assets/grandpa.png"
-import avatarFem from "../../assets/grandmother.png"
+import avatarMasc from "../../assets/young-male.svg"
+import avatarFem from "../../assets/young-female.svg"
 import { FaHeart } from "react-icons/fa"
 import { curtidaMutation } from "../../utils/graphql/mutations/curtidaMutations"
 import { useState } from "react"
@@ -14,38 +13,33 @@ import {
   CarouselPrevious,
 } from "../../components/ui/carousel"
 import Loader from "../../layout/loader/Loader"
-import { Title } from "../cuidadores/Cuidadores"
+import { cuidadoresQueryGql } from "../../utils/graphql/queries/cuidadorQueries"
+import type { Curtida } from "../idosos/Idosos"
 
-export interface Curtida {
-  id: string
-  cuidador: {
-    id: string
-    nome: string
-  }
-  idoso: {
-    id: string
-    nome: string
-  }
-  isMatch: boolean
-}
-export interface Idoso {
+export interface Cuidador {
   id: string
   nome: string
   sexo: string
   curtidas: Curtida[]
 }
 
-export interface IdososQuery {
-  listarIdosos: Idoso[]
+export interface CuidadoresQuery {
+  listarCuidadores: Cuidador[]
 }
 
-export const Idosos = () => {
+export const Title = ({ text }: { text: string }) => (
+  <p className="text-6xl text-center bg-primary p-4 text-white font-bold">
+    {text}
+  </p>
+)
+
+export const Cuidadores = () => {
   const [selected, setSelected] = useState(false)
-  const { data: { listarIdosos } = {}, loading } =
-    useQuery<IdososQuery>(idososQueryGql)
+  const { data: { listarCuidadores } = {}, loading } =
+    useQuery<CuidadoresQuery>(cuidadoresQueryGql)
 
   const [curtirMutation] = useMutation(curtidaMutation, {
-    refetchQueries: [{ query: idososQueryGql }],
+    refetchQueries: [{ query: cuidadoresQueryGql }],
   })
 
   const curtir = async ({
@@ -68,25 +62,25 @@ export const Idosos = () => {
 
   return (
     <div className="flex flex-col justify-center items-center gap-10 h-full">
-      <Title text="Idosos" />
+      <Title text="Cuidadores" />
       <Carousel className="max-w-[40rem]">
         <CarouselContent>
-          {listarIdosos?.map((idoso) => (
+          {listarCuidadores?.map((cuidador) => (
             <CarouselItem className="flex flex-col gap-8 items-center">
-              {idoso.sexo === "Masculino" ? (
+              {cuidador.sexo === "Masculino" ? (
                 <img src={avatarMasc} className="size-[15rem]" />
               ) : (
                 <img src={avatarFem} className="size-[15rem]" />
               )}
               <div className="flex flex-col items-center gap-3">
-                <p className="text-5xl mb-5 font-bold">{idoso.nome}</p>
+                <p className="text-5xl mb-5 font-bold">{cuidador.nome}</p>
                 <p className="text-3xl">Brasília - DF</p>
-                <p className="text-2xl">70 anos</p>
+                <p className="text-2xl">25 anos</p>
               </div>
-              {idoso.curtidas.some((curtida) => curtida.cuidador.id === "1") ? (
+              {cuidador.curtidas.some((curtida) => curtida.idoso.id === "1") ? (
                 <button
                   onClick={() => {
-                    curtir({ cuidadorId: "1", idosoId: idoso.id })
+                    curtir({ idosoId: "1", cuidadorId: cuidador.id })
                     setSelected(!selected)
                   }}
                   className="hover:text-red-500 text-4xl cursor-pointer ease-in-out duration-300 transition-all"
@@ -97,7 +91,7 @@ export const Idosos = () => {
               ) : (
                 <button
                   onClick={() => {
-                    curtir({ cuidadorId: "1", idosoId: idoso.id })
+                    curtir({ idosoId: "1", cuidadorId: cuidador.id })
                     setSelected(!selected)
                   }}
                   className="hover:text-red-500 text-4xl cursor-pointer ease-in-out duration-300 transition-all"
