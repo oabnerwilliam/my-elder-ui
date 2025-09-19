bc9a88d0cad2099557554b76ca71a20cd13d5a2f
oabnerwilliam <abnerwilliam026@gmail.com>
2025-09-18 23:27:00 -0300

    estilizando a home

--- src/screens/home/HomePage.tsx
+++ src/screens/home/HomePage.tsx
@@ -1,3 +1,16 @@
+import src from "../../assets/avatar-home.svg"
+
 export const HomePage = () => {
-  return <h1>Bem-vindo</h1>
+  return (
+    <div className="flex flex-col h-full items-center justify-center gap-[3rem]">
+      <p className="text-6xl font-bold">
+        Bem-Vindo ao <span className="bg-primary p-4 text-white">MyElder!</span>
+      </p>
+      <p className="text-2xl">
+        A sua rede social para{" "}
+        <span className="bg-primary p-1 text-white">cuidados com idosos!</span>
+      </p>
+      <img src={src} className="size-[25rem]" />
+    </div>
+  )
 }
