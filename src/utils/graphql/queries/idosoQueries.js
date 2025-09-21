c52492b88e24f86a027a111b1c2f034a4bf869c5
oabnerwilliam <abnerwilliam026@gmail.com>
2025-09-20 22:59:52 -0300

    atualizando as queries de idoso

--- src/utils/graphql/queries/idosoQueries.js
+++ src/utils/graphql/queries/idosoQueries.js
@@ -1,16 +1,34 @@
 import gql from "graphql-tag"
 import { curtidaFragment } from "./curtidaQueries"
+import { usuarioFragment } from "./usuarioQueries"
+
+export const idosoFragment = gql`
+  fragment idosoFragment on Idoso {
+    id
+    nome
+    nomeUsuario
+    senha
+    sexo
+    localizacao
+    dataNascimento
+    idade
+  }
+`
+
+export const idosoQueryGql = gql`
+  query idosoQuery($id: ID!) {
+    idoso(id: $id) {
+      ...idosoFragment
+    }
+  }
+  ${idosoFragment}
+`
 
 export const idososQueryGql = gql`
   query {
-    listarIdosos {
-      id
-      nome
-      sexo
-      curtidas {
-        ...curtidaFragment
-      }
+    idosos {
+      ...idosoFragment
     }
   }
-  ${curtidaFragment}
+  ${idosoFragment}
 `
